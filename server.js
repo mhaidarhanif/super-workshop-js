3cb948f2c20b9fa6a0aa5461a6e0681d0432f340
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-28 21:21:16 +0700

    express setup

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 'use strict'
 
+// -----------------------------------------------------------------------------
+// NODE MODULES
+// -----------------------------------------------------------------------------
+
+// Express dependencies
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Initiate Express
+const app = express()
+const router = app.Router()
+
+// -----------------------------------------------------------------------------
+// APP CONFIGURATION
+// -----------------------------------------------------------------------------
