10785d25ee37f087419c76b7f5b05af54d18d6a9
M Haidar Hanif <git@mhaidarhanif.com>
2016-11-02 15:21:49 +0700

    get data

--- client/index.js
+++ client/index.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-  $('#description').html("jQuery is active!")
+  let $description = $('#description')
+
+  // GET DATA
+  $.getJSON(`http://localhost:3000/books`, function (data) {
+    console.log(data)
+    $description.html(`${data[0].id}: ${data[0].name} ($${data[0].price})`)
+  })
+
 })
