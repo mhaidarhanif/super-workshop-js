24b0298eaacc46d1cae8770a96ae70c65e07a86f
M Haidar Hanif <git@mhaidarhanif.com>
2016-11-04 18:29:01 +0700

    Separate view and logic between end user and admin
    Using Handlebars, but still looks unstructured
    Let's try Vue.js

--- client/scripts/admin/index.js
+++ client/scripts/admin/index.js
@@ -1,18 +1,13 @@
 /*global $, jQuery, EJS, Handlebars, Router */
 $(document).ready(function () {
-  // Configuration
   const api = `http://localhost:3000/api`
 
-  // jQuery objects
-  let $description = $('#description')
-
-  // Handlebars templates
-  let $listOfBooks = $('#books-list-template').html()
-
-  // ---------------------------------------------------------------------------
+  let $listOfBooks = $('#books-list-template-admin').html()
 
   // Get all data from server
   $.getJSON(`${api}/books`, (data) => {
+    // console.log(data)
+    // console.log($listOfBooks)
     let test = Handlebars.compile($listOfBooks)
     $('#books-list-table').append(test(data))
   })
