5288f520de00b124973b29d6100622fbc32be06d
M Haidar Hanif <git@mhaidarhanif.com>
2016-11-04 17:31:46 +0700

    Trying out EJS and Handlebars for client-side

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,10 +1,12 @@
+/*global $, jQuery, EJS, Handlebars, Router */
 $(document).ready(function () {
+  let api = `http://localhost:3000/api`
   let $description = $('#description')
 
-  // GET DATA
-  $.getJSON(`http://localhost:3000/books`, function (data) {
-    console.log(data)
-    $description.html(`${data[0].id}: ${data[0].name} ($${data[0].price})`)
+  // Get all data from server
+  $.getJSON(`${api}/books`, function (data) {
+    // console.log(data)
+    $description.html(`${data[0].isbn}: ${data[0].name} ($${data[0].price})`)
   })
 
 })
