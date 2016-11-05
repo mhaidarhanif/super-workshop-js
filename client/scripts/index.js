3ce49a3a01562e216dcde723ad7a701ea0593c80
M Haidar Hanif <git@mhaidarhanif.com>
2016-11-06 05:33:46 +0700

    Cleaner templates

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -4,26 +4,33 @@ $(document).ready(function () {
   const api = `http://localhost:3000/api`
   const $menuPanel = $('#menu-panel-template').html()
   const $booksListTable = $('#books-list-table')
+  const $booksListHeader = $('#books-list-header')
+  const $booksListHeaderTemplate = $('#books-list-header-template').html()
   const $booksListContent = $('#books-list-content')
-  const $booksList = $('#books-list-template').html()
+  const $booksListContentTemplate = $('#books-list-content-template').html()
 
-  function getAccountProfile() {
+  function getUser() {
     let token = localStorage.getItem('token')
     if (!token) return {}
     else {
-      let profile = jwt_decode(token)
-      return profile
+      let user = jwt_decode(token)
+      return user
     }
   }
 
-  function compileBooksList(data) {
-    let list = Handlebars.compile($booksList)
-    $booksListContent.append(list(data))
+  function compileBooksHeader() {
+    let template = Handlebars.compile($booksListHeaderTemplate)
+    $booksListHeader.append(template({ user: getUser() }))
+  }
+
+  function compileBooksContent(data) {
+    let template = Handlebars.compile($booksListContentTemplate)
+    $booksListContent.append(template({ user: getUser(), books: data }))
   }
 
   function getDataFromAPI() {
     $.getJSON(`${api}/books`, (data) => {
-      compileBooksList(data)
+      compileBooksContent(data)
     })
   }
 
@@ -38,19 +45,19 @@ $(document).ready(function () {
         dataType: 'json'
       })
       .done((data) => {
-        // console.log(data)
-        compileBooksList(data)
+        compileBooksContent(data)
       })
       .fail((err) => {
         errorMessage(true, 'Something wrong')
       })
   }
 
-  // Append menu panel based on profile
-  let profile = getAccountProfile()
-  $('#menu').append(Handlebars.compile($menuPanel)(profile))
+  // Append menu panel based on user
+  let user = getUser()
+  $('#menu').append(Handlebars.compile($menuPanel)(user))
 
   // Get initial data
+  compileBooksHeader()
   getDataFromAPI()
 
   // Search input
