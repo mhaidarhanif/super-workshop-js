1c6154503df03e65f9d1c81ec6b428aca1db15ef
M Haidar Hanif <git@mhaidarhanif.com>
2016-11-14 00:30:55 +0700

    Add or pdate capability, even from data table
    Combine POST or PUT request based on ISBN availability
    Streamline class and selected element to be used with editor

--- client/scripts/admin.js
+++ client/scripts/admin.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   const $bookEditorTemplate = $('#book-editor-template').html()
 
   compileMenu()
-  compileEmptyBookEditor()
+  compileBookEditor()
   compileBooksHeader()
   getDataFromAPI()
 
@@ -15,7 +15,7 @@ $(document).ready(function () {
   // VIEW+ACTION: Books Action
   // ---------------------------------------------------------------------------
 
-  function compileEmptyBookEditor() {
+  function compileBookEditor() {
 
     $bookEditor.append(Handlebars.compile($bookEditorTemplate))
     $bookEditor.hide()
@@ -31,27 +31,32 @@ $(document).ready(function () {
     // onSubmit, post a new book
     $('#book-editor-form').submit((e) => {
       e.preventDefault()
+
+      let urlInput = $('#book-editor-form').attr('action')
+      let isbnInput = $('#book-editor-form input[name=isbn]').val()
+      let nameInput = $('#book-editor-form input[name=name]').val()
+      let priceInput = $('#book-editor-form input[name=price]').val()
       let dataInput = {
-        isbn: $('#book-editor-form input[name=isbn]').val(),
-        name: $('#book-editor-form input[name=name]').val(),
-        price: $('#book-editor-form input[name=price]').val(),
+        isbn: isbnInput,
+        name: nameInput,
+        price: priceInput
       }
+
       $.ajax({
-          method: 'POST',
-          url: $('#book-editor-form').attr('action'),
-          data: dataInput,
-          headers: { 'Authorization': `Bearer ${Auth.getToken()}` }
-        })
-        .done((data) => {
-          getDataFromAPI()
-        })
-        .fail((xhr, textStatus, err) => {
-          alert(JSON.parse(xhr.responseText).message)
-        })
+        url: `${urlInput}/${isbnInput}`,
+        success: (data) => {
+          modifyBook('PUT', `${urlInput}/${isbnInput}`, dataInput)
+        },
+        error: (xhr, textStatus, err) => {
+          // console.log(JSON.parse(xhr.responseText).message)
+          modifyBook('POST', urlInput, dataInput)
+        }
+      })
+
     })
 
     // onClick, cancel the form
-    $('#book-editor-form input[name=addCancel]').on('click', (e) => {
+    $('#book-editor-form input[name=cancel]').on('click', (e) => {
       $bookEditor.hide()
     })
 
@@ -106,32 +111,41 @@ $(document).ready(function () {
 
   }
 
-  // ---------
-  // Book List
+  // -------------------
+  // Book List Modifiers
 
-  // Update book by ISBN. But first, get its data.
-  $booksListTable.on('click', 'td.update', function () {
-    // $('#book-editor').show()
-    let isbn = $(this).attr('data-update')
-    $.getJSON({
-        url: `${api}/books/${isbn}`,
-        headers: { 'Authorization': Auth.getToken() }
+  // Add or update book based on availability
+  function modifyBook(methodInput, urlInput, dataInput) {
+    // console.log(methodInput, urlInput, dataInput)
+    $.ajax({
+        method: methodInput,
+        url: urlInput,
+        data: dataInput,
+        headers: { 'Authorization': `Bearer ${Auth.getToken()}` }
       })
       .done((data) => {
-        compileBookEditor(data)
-        $('#book-isbn').val(data.isbn)
-        $('#book-name').val(data.name)
-        $('#book-price').val(data.price)
-        $('#book-editor').show()
-      }).fail((err) => {
-        console.log('Error', err)
+        getDataFromAPI()
       })
+      .fail((xhr, textStatus, err) => {
+        alert(JSON.parse(xhr.responseText).message)
+      })
+  }
+
+  // Update book by ISBN, only fill the input by its value
+  $booksListTable.on('click', 'td.update', function () {
+    let isbn = $(this).siblings(".isbn").html()
+    let name = $(this).siblings(".name").html()
+    let price = $(this).siblings(".price").children("span").html()
+
+    $('#book-editor').show()
+    $('#input-book-isbn').val(isbn)
+    $('#input-book-name').val(name)
+    $('#input-book-price').val(price)
   })
 
   // Remove book by ISBN
-
   $booksListTable.on('click', 'td.remove', function () {
-    // $('#book-editor').hide()
+    $('#book-editor').hide()
     let isbn = $(this).attr('data-remove')
     if (confirm(`Sure to delete book with ISBN ${isbn}?`)) {
       $.ajax({
