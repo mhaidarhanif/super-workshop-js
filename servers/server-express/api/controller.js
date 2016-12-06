ebb723ba15a9bbadabad28f9ef4bbe56e0c6614e
M Haidar Hanif <git@mhaidarhanif.com>
2016-12-06 13:31:38 +0700

    Streamline info from server root: home, api, ping
    * Utilize process.env.NAME
    * Ping that getClientIp(req) by request-ip

--- servers/server-express/api/controller.js
+++ servers/server-express/api/controller.js
@@ -1,25 +1,54 @@
+const reqip = require('request-ip')
+
 module.exports = {
 
+  // ---------------------------------------------------------------------------
+  // PUBLIC
+  // ---------------------------------------------------------------------------
+
   /*
-    Home
-  */
+   * Home
+   */
   home: (req, res) => {
-    res.send('Home')
+    res.json({
+      id: 'home',
+      m: `You might want to check ${process.env.HOST}:${process.env.PORT}/api instead.`
+    })
   },
 
   /*
-    API intro
-  */
+   * API
+   */
   api: (req, res) => {
-    res.send('API')
+    res.json({
+      name: 'Super Workshop JS',
+      description: `Welcome to the API that run with ${process.env.NAME}! This a quick help for you to consume the API.`,
+      documentation: 'https://github.com/mhaidarh/super-workshop-js#readme',
+      endpoints: {
+        auth: '/auth',
+        accounts: '/api/accounts',
+        books: '/api/books'
+      },
+      codes: {
+        d: 'd indicates previous data that might already removed.',
+        e: 'e indicates an error explanation.',
+        i: 'i indicates an info or warning.',
+        id: 'id indicates an info and error type.',
+        m: 'm indicates a readable messsage from the API developer.',
+        s: 's indicates a success flag, whether true or false.'
+      }
+    })
   },
 
   /*
-    Ping
-  */
+   * Ping
+   */
   ping: (req, res) => {
-    console.log('ping')
-    res.json({ 'message': 'PONG!' })
+    console.log(`ping from ${reqip.getClientIp(req)}`)
+    res.json({
+      id: 'ping_pong',
+      m: `ping from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`
+    })
   }
 
 }
