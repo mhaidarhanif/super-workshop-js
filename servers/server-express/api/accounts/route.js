f2af09614eb28c08fc4f819ad5665083adf6460d
M Haidar Hanif <git@mhaidarhanif.com>
2016-12-06 12:55:40 +0700

    Document accounts routes

--- servers/server-express/api/accounts/route.js
+++ servers/server-express/api/accounts/route.js
@@ -5,15 +5,23 @@ const api = require('./controller')
 const auth = require('../auth/controller')
 
 // -----------------------------------------------------------------------------
-// api/accounts
+// ADMINISTRATIVE
 // -----------------------------------------------------------------------------
 
-// ADMINISTRATIVE
+// Get all accounts
 router.get('/', auth.isAdmin, api.getAccounts)
+// Seed initial accounts
 router.post('/actions/seed', auth.isAdmin, api.seedAccounts)
+// Delete all accounts
 router.delete('/actions/delete', auth.isAdmin, api.deleteAccounts)
 
+// -----------------------------------------------------------------------------
 // PUBLIC
+// -----------------------------------------------------------------------------
+
+// Get account by accountId
 router.get('/:accountId', auth.isAuthenticated, api.getAccountProfileById)
+
+// -----------------------------------------------------------------------------
 
 module.exports = router
