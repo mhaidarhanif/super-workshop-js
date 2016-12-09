606a172577ef023b0a2948c1d9eccd82dd27e688
M Haidar Hanif <git@mhaidarhanif.com>
2016-12-09 23:34:30 +0700

    Modularize credential configs

--- servers/server-express/config/credential.js
+++ servers/server-express/config/credential.js
@@ -1,35 +1,39 @@
 const connection = require('./connection')
 const url = connection.URL
 
+const SECRET = {
+  JWT: '',
+  SESSION: ''
+}
+
+const GITHUB = {
+  CLIENT_ID: '',
+  CLIENT_SECRET: '',
+  CALLBACK: `${url}/auth/github/callback`
+}
+
+const FACEBOOK = {
+  APP_ID: '',
+  APP_SECRET: '',
+  CALLBACK: `${url}/auth/facebook/callback`
+}
+
+const TWITTER = {
+  API_KEY: '',
+  API_SECRET: '',
+  CALLBACK: `${url}/auth/twitter/callback`
+}
+
+const GOOGLE = {
+  CLIENT_ID: '',
+  CLIENT_SECRET: '',
+  CALLBACK: `${url}/auth/google/callback`
+}
+
 module.exports = {
-
-  SECRET: {
-    JWT: '',
-    SESSION: ''
-  },
-
-  GITHUB: {
-    CLIENT_ID: '',
-    CLIENT_SECRET: '',
-    CALLBACK: `${url}/auth/github/callback`
-  },
-
-  FACEBOOK: {
-    APP_ID: '',
-    APP_SECRET: '',
-    CALLBACK: `${url}/auth/facebook/callback`
-  },
-
-  TWITTER: {
-    API_KEY: '',
-    API_SECRET: '',
-    CALLBACK: `${url}/auth/twitter/callback`
-  },
-
-  GOOGLE: {
-    CLIENT_ID: '',
-    CLIENT_SECRET: '',
-    CALLBACK: `${url}/auth/google/callback`
-  }
-
+  SECRET,
+  GITHUB,
+  FACEBOOK,
+  TWITTER,
+  GOOGLE
 }
