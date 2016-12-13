ef767df288c9f6b6705f9a42dedd63da98f7214f
M Haidar Hanif <git@mhaidarhanif.com>
2016-12-13 10:16:23 +0700

    Utilize process.env for credential.schema

--- servers/server-express/config/credential.schema.js
+++ servers/server-express/config/credential.schema.js
@@ -2,38 +2,38 @@ const connection = require('./connection')
 const url = connection.URL
 
 const SECRET = {
-  JWT: '',
-  SESSION: ''
+  JWT: process.env.JWT_SECRET || 'change_this',
+  SESSION: process.env.SESSION_SECRET || 'change_this'
 }
 
 const API_KEY = {
-  DEV: '',
-  TEST: '',
-  STAGING: '',
-  PRODUCTION: ''
+  DEV: process.env.API_KEY_DEV || 'change_this',
+  TEST: process.env.API_KEY_TEST || 'change_this',
+  STAGING: process.env.API_KEY_STAGING || 'change_this',
+  PRODUCTION: process.env.API_KEY_PRODUCTION || 'change_this'
 }
 
 const GITHUB = {
-  CLIENT_ID: '',
-  CLIENT_SECRET: '',
+  CLIENT_ID: process.env.GITHUB_ID || 'change_this',
+  CLIENT_SECRET: process.env.GITHUB_SECRET || 'change_this',
   CALLBACK: `${url}/auth/github/callback`
 }
 
 const FACEBOOK = {
-  APP_ID: '',
-  APP_SECRET: '',
+  APP_ID: process.env.FACEBOOK_ID || 'change_this',
+  APP_SECRET: process.env.FACEBOOK_SECRET || 'change_this',
   CALLBACK: `${url}/auth/facebook/callback`
 }
 
 const TWITTER = {
-  API_KEY: '',
-  API_SECRET: '',
+  API_KEY: process.env.TWITTER_KEY || 'change_this',
+  API_SECRET: process.env.TWITTER_SECRET || 'change_this',
   CALLBACK: `${url}/auth/twitter/callback`
 }
 
 const GOOGLE = {
-  CLIENT_ID: '',
-  CLIENT_SECRET: '',
+  CLIENT_ID: process.env.GOOGLE_ID || 'change_this',
+  CLIENT_SECRET: process.env.GOOGLE_SECRET || 'change_this',
   CALLBACK: `${url}/auth/google/callback`
 }
 
