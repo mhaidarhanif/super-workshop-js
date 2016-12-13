94691e64376ad1c46b0a21fd4392fbb1d990fb13
M Haidar Hanif <git@mhaidarhanif.com>
2016-12-13 14:13:25 +0700

    server-express: Refine index.js info

--- servers/server-express/index.js
+++ servers/server-express/index.js
@@ -1,13 +1,11 @@
 const server = require('./server')
 
-const url = process.env.URL
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || '3000'
-
-console.log(process.env.MONGODB_URI)
+const url = `${host}:${port}`
 
 server.listen(port, host, (err) => {
-  if (err) console.log(err)
+  if (err) console.error(err)
   console.log(`[i] ${process.env.NAME} is running on ${url}`)
   console.log(`[i] NODE_ENV: ${process.env.NODE_ENV}`)
   console.log(`[i] MONGODB_URI: ${process.env.MONGODB_URI}`)
