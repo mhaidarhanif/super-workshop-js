c01de1fb9ad92b2d449ae90f5c3ef2041a55456c
M Haidar Hanif <git@mhaidarhanif.com>
2016-12-13 13:01:44 +0700

    test-chai: Test essential server-express

---
+++ tests/test-chai/api/index.js
@@ -0,0 +1,106 @@
+require('dotenv-extended').load()
+const server = require(process.env.SERVER_DIR + 'server')
+
+const chai = require('chai')
+const chaiHTTP = require('chai-http')
+const expect = chai.expect
+chai.use(chaiHTTP)
+
+// -----------------------------------------------------------------------------
+
+describe('server-express', function () {
+  it('expect ok to load', function (done) {
+    expect(server).to.be.ok
+    done()
+  })
+
+  // ---------------------------------------------------------------------------
+
+  describe('root', () => {
+    var res
+
+    before(() => {
+      chai.request(server).get('/').then(response => { res = response })
+    })
+
+    it('execute request', (done) => {
+      done()
+    })
+
+    it('expect json object that contains specific keys', (done) => {
+      expect(res.body).to.be.an('object')
+      expect(res.body).to.have.all.keys('id', 'name', 'm', 'host', 'port', 'url')
+      done()
+    })
+  })
+
+  // ---------------------------------------------------------------------------
+
+  describe('ping', () => {
+    var res
+
+    before(() => {
+      chai.request(server).get('/ping').then(response => { res = response })
+    })
+
+    it('execute request', (done) => {
+      done()
+    })
+
+    it('expect json object that contains specific keys', (done) => {
+      expect(res.body).to.be.an('object')
+      expect(res.body).to.have.all.keys('id', 'm')
+      done()
+    })
+
+    it('expect info about ping', (done) => {
+      expect(res.body).to.have.property('id', 'ping_pong')
+      expect(res.body).to.have.property('m').to.include('ping')
+      done()
+    })
+  })
+
+  // ---------------------------------------------------------------------------
+
+  describe('api', function () {
+    var res
+
+    before(() => {
+      chai.request(server).get('/api').then(response => { res = response })
+    })
+
+    it('execute request', (done) => {
+      done()
+    })
+
+    it('expect json object that contains specific keys', (done) => {
+      expect(res.body).to.be.an('object')
+      expect(res.body).to.have.all.keys('id', 'name', 'description', 'documentation', 'endpoints', 'codes')
+      done()
+    })
+
+    it('expect info about api', (done) => {
+      expect(res.body).to.have.property('id', 'api')
+      expect(res.body).to.have.property('name', process.env.NAME)
+      done()
+    })
+
+    it('expect  info about API endpoints', (done) => {
+      // console.log(res.body.endpoints)
+      expect(res.body.endpoints).to.have.property('/auth').to.include('Authentication')
+      expect(res.body.endpoints).to.have.property('/api/accounts').to.include('Accounts')
+      expect(res.body.endpoints).to.have.property('/api/books').to.include('Books')
+      done()
+    })
+
+    it('expect give info about API endpoints', (done) => {
+      expect(res.body.codes).to.have.property('d').to.include('data')
+      expect(res.body.codes).to.have.property('e').to.include('error')
+      expect(res.body.codes).to.have.property('i').to.include('info')
+      expect(res.body.codes).to.have.property('id').to.include('type')
+      expect(res.body.codes).to.have.property('m').to.include('developer')
+      expect(res.body.codes).to.have.property('s').to.include('success')
+      done()
+    })
+  })
+})
