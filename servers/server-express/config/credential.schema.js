0d96bf44cab1301d84c157c0b64d9de0d0ddd4c5
M Haidar Hanif <git@mhaidarhanif.com>
2016-12-19 21:27:52 +0700

    Mail system credential environment
    Mailgun + SendGrid

--- servers/server-express/config/credential.schema.js
+++ servers/server-express/config/credential.schema.js
@@ -14,33 +14,51 @@ const API_KEY = {
   PRODUCTION: process.env.API_KEY_PRODUCTION || 'change_this'
 }
 
+const MAIL = {
+  MAILGUN: {
+    API_KEY: process.env.MAILGUN_API_KEY || 'change_this',
+    DOMAIN: process.env.MAILGUN_DOMAIN || 'change_this',
+    SENDER: process.env.MAILGUN_SENDER || 'change_this'
+  },
+  SENDGRID: {
+    API_KEY: process.env.SENDGRID_API_KEY || 'change_this',
+    DOMAIN: process.env.SENDGRID_DOMAIN || 'change_this',
+    SENDER: process.env.SENDGRID_SENDER || 'change_this'
+  }
+}
+
 const GITHUB = {
   CLIENT_ID: process.env.GITHUB_ID || 'change_this',
   CLIENT_SECRET: process.env.GITHUB_SECRET || 'change_this',
+  SCOPE: ['id'],
   CALLBACK: `${url}/auth/github/callback`
 }
 
 const FACEBOOK = {
   APP_ID: process.env.FACEBOOK_ID || 'change_this',
   APP_SECRET: process.env.FACEBOOK_SECRET || 'change_this',
+  SCOPE: ['id', 'displayName', 'photos', 'email'],
   CALLBACK: `${url}/auth/facebook/callback`
 }
 
 const TWITTER = {
   API_KEY: process.env.TWITTER_KEY || 'change_this',
   API_SECRET: process.env.TWITTER_SECRET || 'change_this',
+  SCOPE: ['id'],
   CALLBACK: `${url}/auth/twitter/callback`
 }
 
 const GOOGLE = {
   CLIENT_ID: process.env.GOOGLE_ID || 'change_this',
   CLIENT_SECRET: process.env.GOOGLE_SECRET || 'change_this',
+  SCOPE: ['id'],
   CALLBACK: `${url}/auth/google/callback`
 }
 
 module.exports = {
   SECRET,
   API_KEY,
+  MAIL,
   GITHUB,
   FACEBOOK,
   TWITTER,
