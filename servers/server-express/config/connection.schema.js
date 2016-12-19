c6585b312b21252d6a27a65451c9377c58aa5d10
M Haidar Hanif <git@mhaidarhanif.com>
2016-12-19 21:24:17 +0700

    Streamline connection environment

--- servers/server-express/config/connection.schema.js
+++ servers/server-express/config/connection.schema.js
@@ -1,9 +1,22 @@
 const PROTOCOL = 'http://'
-const HOST = 'localhost'
-const PORT = '3000'
-const URL = `${PROTOCOL}${HOST}:${PORT}`
+
+const HOST = {
+  SERVER: process.env.HOST_SERVER || 'localhost',
+  CLIENT: process.env.HOST_CLIENT || 'localhost'
+}
+
+const PORT = {
+  SERVER: process.env.PORT_SERVER || 3000,
+  CLIENT: process.env.PORT_CLIENT || 8000
+}
+
+const URL = {
+  SERVER: `${PROTOCOL}${HOST.SERVER}:${PORT.SERVER}`,
+  CLIENT: `${PROTOCOL}${HOST.CLIENT}:${PORT.CLIENT}`
+}
 
 module.exports = {
+  PROTOCOL,
   HOST,
   PORT,
   URL
