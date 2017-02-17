3f0fa46f75e71720b646296b86329f31a26fea6f
M Haidar Hanif <git@mhaidarhanif.com>
2017-02-17 19:10:00 +0700

    Refine test-chai for books + posts

--- tests/test-chai/api/books.js
+++ tests/test-chai/api/books.js
@@ -1,31 +1,40 @@
 require('dotenv-extended').load()
 const server = require(process.env.SERVER_DIR + 'server')
 
+// -----------------------------------------------------------------------------
+
 const chai = require('chai')
 const chaiHTTP = require('chai-http')
+const chaiArrays = require('chai-arrays')
 const expect = chai.expect
+
+chai.use(chaiArrays)
 chai.use(chaiHTTP)
 
 // -----------------------------------------------------------------------------
 
-describe('books', () => {
-  let res
+const resource = `book`
+const resources = `${resource}s`
+const endpoint = `/api/${resources}`
 
+// -----------------------------------------------------------------------------
+
+describe('books', () => {
   // -------------------------------------------------------------------------
   describe('seed', () => {
     // -------------------------------------------------------------------------
     describe('some books', () => {
       it('expect to seed', (done) => {
-        chai.request(server).post('/api/books/actions/seed')
-        .set('X-API-Key', process.env.API_KEY_SETUP)
-        .then(res => {
-          expect(res.body).to.be.an('object')
-          expect(res.body).to.have.all.keys('books')
-          expect(res.body).to.have.deep.property('books.id').to.include('success')
-          expect(res.body).to.have.deep.property('books.m').to.include('some')
-          done()
-        })
-        .catch(err => { done(err) })
+        chai.request(server).post(`${endpoint}/actions/seed`)
+          .set('X-API-Key', process.env.API_KEY_SETUP)
+          .then(res => {
+            expect(res.body).to.be.an('object')
+            expect(res.body).to.have.all.keys('books')
+            expect(res.body).to.have.deep.property('books.id').to.include('success')
+            expect(res.body).to.have.deep.property('books.m').to.include('some')
+            done()
+          })
+          .catch(err => { done(err) })
       })
     })
   })
--- tests/test-chai/api/posts.js
+++ tests/test-chai/api/posts.js
@@ -1,20 +1,31 @@
 require('dotenv-extended').load()
 const server = require(process.env.SERVER_DIR + 'server')
 
+// -----------------------------------------------------------------------------
+
 const chai = require('chai')
 const chaiHTTP = require('chai-http')
+const chaiArrays = require('chai-arrays')
 const expect = chai.expect
+
+chai.use(chaiArrays)
 chai.use(chaiHTTP)
 
 // -----------------------------------------------------------------------------
 
-describe.skip('posts', () => {
+const resource = `post`
+const resources = `${resource}s`
+const endpoint = `/api/${resources}`
+
+// -----------------------------------------------------------------------------
+
+describe.only('posts', () => {
   // -------------------------------------------------------------------------
-  describe('seed', () => {
+  describe('SEED', () => {
     // -------------------------------------------------------------------------
     describe('some posts', () => {
-      it('expect to seed', (done) => {
-        chai.request(server).post('/api/posts/actions/seed')
+      it('expect to seed some posts', (done) => {
+        chai.request(server).post(`${endpoint}/actions/seed`)
           .set('X-API-Key', process.env.API_KEY_SETUP)
           .then(res => {
             expect(res.body).to.be.an('object')
@@ -29,18 +40,85 @@ describe.skip('posts', () => {
       })
     })
   })
+
   // -------------------------------------------------------------------------
-  describe('get', () => {
+  describe.skip('POST', () => {
+    let res
+    // -------------------------------------------------------------------------
+    describe('a post', () => {
+      it('execute request', (done) => {
+        chai.request(server).post(`${endpoint}`)
+          .set({
+            title: 'Hello World',
+            content: 'Hello people of the world'
+          })
+          .then(response => {
+            res = response
+            done()
+          })
+          .catch(err => {
+            done(err)
+          })
+      })
+
+      it('expect to post a post', (done) => {
+        expect(res.body).to.be.an('object')
+        expect(res.body).to.have.property('_id')
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('title')
+        expect(res.body).to.have.property('content')
+        expect(res.body).to.have.property('status')
+        expect(res.body).to.have.property('createdBy')
+        expect(res.body).to.have.property('updatedBy')
+        expect(res.body).to.have.property('createdAt')
+        expect(res.body).to.have.property('updatedAt')
+      })
+
+      it('expect the newly created post to be exact', (done) => {
+
+      })
+    })
+  })
+
+  // -------------------------------------------------------------------------
+  describe('GET', () => {
     // -------------------------------------------------------------------------
     describe('some posts', () => {
-      it('expect json object that contains specific keys', (done) => {
-        chai.request(server).get('/api/posts')
-          .set('X-API-Key', process.env.API_KEY_SETUP)
+      it('expect some posts contain specific keys', (done) => {
+        chai.request(server).get(`${endpoint}`)
           .then(res => {
-            expect(res.body).to.be.an('object')
-            expect(res.body).to.have.all.keys('posts')
-            expect(res.body).to.have.deep.property('posts.id').to.include('success')
-            expect(res.body).to.have.deep.property('posts.m').to.include('some')
+            expect(res.body).to.be.an('array')
+            expect(res.body[0]).to.have.property('id')
+            expect(res.body[0]).to.have.property('title')
+            expect(res.body[0]).to.have.property('content')
+            done()
+          })
+          .catch(err => {
+            done(err)
+          })
+      })
+      it('expect some posts have a specific length', (done) => {
+        chai.request(server).get(`${endpoint}`)
+          .then(res => {
+            expect(res.body).to.be.an('array')
+            expect(res.body).to.have.length.below(11)
+            done()
+          })
+          .catch(err => {
+            done(err)
+          })
+      })
+    })
+
+    // -------------------------------------------------------------------------
+    describe('all posts', () => {
+      it('expect all posts contains specific keys', (done) => {
+        chai.request(server).get(`${endpoint}/all`)
+          .then(res => {
+            expect(res.body).to.be.an('array')
+            expect(res.body[0]).to.have.property('id')
+            expect(res.body[0]).to.have.property('title')
+            expect(res.body[0]).to.have.property('content')
             done()
           })
           .catch(err => {
@@ -51,14 +129,44 @@ describe.skip('posts', () => {
 
     // -------------------------------------------------------------------------
     describe('a post', () => {
-      it('expect json object that contains specific keys', (done) => {
-        chai.request(server).get('/api/posts/1')
+      it('expect a post contains specific keys', (done) => {
+        chai.request(server).get(`${endpoint}`)
+          .then(res => {
+            chai.request(server).get(`${endpoint}/${res.body[0].id}`)
+              .then(res => {
+                expect(res.body).to.be.an('object')
+                expect(res.body).to.have.property('_id')
+                expect(res.body).to.have.property('id')
+                expect(res.body).to.have.property('title')
+                expect(res.body).to.have.property('content')
+                expect(res.body).to.have.property('status')
+                expect(res.body).to.have.property('createdBy')
+                expect(res.body).to.have.property('updatedBy')
+                expect(res.body).to.have.property('createdAt')
+                expect(res.body).to.have.property('updatedAt')
+                done()
+              })
+              .catch(err => {
+                done(err)
+              })
+          })
+          .catch(err => {
+            done(err)
+          })
+      })
+    })
+  })
+
+  // -------------------------------------------------------------------------
+  describe.skip('DELETE', () => {
+    // -------------------------------------------------------------------------
+    describe('all posts', () => {
+      it('expect all posts to be empty after deleted', (done) => {
+        chai.request(server).delete(`${endpoint}/actions/delete`)
           .set('X-API-Key', process.env.API_KEY_SETUP)
           .then(res => {
+            console.log(res.body)
             expect(res.body).to.be.an('object')
-            expect(res.body).to.have.all.keys('posts')
-            expect(res.body).to.have.deep.property('posts.id').to.include('success')
-            expect(res.body).to.have.deep.property('posts.m').to.include('some')
             done()
           })
           .catch(err => {
