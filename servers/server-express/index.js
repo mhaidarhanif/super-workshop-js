d8238780df8d710683fad01e20e893e2b22e1a1c
M Haidar Hanif <git@mhaidarhanif.com>
2017-02-22 17:53:55 +0700

    Revamp express server config + modules
    Especially logger using middlewares, based on Node environment

--- servers/server-express/index.js
+++ servers/server-express/index.js
@@ -3,9 +3,15 @@ const server = require('./server')
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 3000
 
+// Load .env if not using pm2
+if (!process.env.PM2_USAGE) {
+  require('dotenv-extended').load()
+}
+
+// Listen on configured host:port
 server.listen(port, host, (err) => {
   if (err) console.error(err)
   console.log(`[i] ${process.env.NAME} is running on ${process.env.URL}`)
-  console.log(`[i] NODE_ENV: ${process.env.NODE_ENV}`)
+  console.log(`[i] NODE_ENV: ${process.env.NODE_ENV || server.settings.env}`)
   console.log(`[i] MONGODB_URI: ${process.env.MONGODB_URI}`)
 })
--- servers/server-express/server.js
+++ servers/server-express/server.js
@@ -4,12 +4,18 @@
 // REQUIRE NODE MODULES
 // -----------------------------------------------------------------------------
 
+// NODE
+const fs = require('fs')
+const path = require('path')
+const http = require('http')
+
 // EXPRESS
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const expressSession = require('express-session')
 const expressValidator = require('express-validator')
-const cors = require('cors')
 
 // INITIALIZE EXPRESS APP
 const app = express()
@@ -17,6 +23,11 @@ const app = express()
 // DATA
 const mongoose = require('mongoose')
 const passport = require('passport')
+
+// LOGGER
+const morgan = require('morgan')
+const winston = require('winston')
+const expressWinston = require('express-winston')
 
 // -----------------------------------------------------------------------------
 // REQUIRE INTERNAL MODULES
@@ -34,24 +45,28 @@ const apiBooks = require('./api/books/route')
 const apiPosts = require('./api/posts/route')
 
 // -----------------------------------------------------------------------------
-// CONFIGURE APP + DATA
+// USE PLUGINS
 // -----------------------------------------------------------------------------
 
 // EXPRESS
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(expressValidator())
-app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
 
-/*
- * MONGODB + MONGOOSE
- * Prevent this DeprecationWarning
- * Mongoose: mpromise (mongoose's default promise library) is deprecated,
- * plug in your own promise library instead:
- * http://mongoosejs.com/docs/promises.html
- */
+// -----------------------------------------------------------------------------
+// USE DATABASE
+// -----------------------------------------------------------------------------
+
+// MONGODB + MONGOOSE
 mongoose.Promise = global.Promise // native Node.js promise
 mongoose.connect(process.env.MONGODB_URI)
+
+// -----------------------------------------------------------------------------
+// USE SESSION
+// -----------------------------------------------------------------------------
 
 // EXPRESS SESSION
 app.use(expressSession({
@@ -61,7 +76,51 @@ app.use(expressSession({
 }))
 
 // -----------------------------------------------------------------------------
-// CONFIGURE ROUTES
+// USE LOGGER
+// -----------------------------------------------------------------------------
+
+if (app.get('env') === 'dev') {
+  // Log into file
+  const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'), {flags: 'a'})
+  app.use(morgan('combined', {stream: accessLogStream}))
+
+  // Log request
+  app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console({
+        json: false,
+        colorize: true
+      })
+    ],
+    meta: true, // log the meta data about the request
+    msg: 'HTTP {{req.method}} {{req.url}}', // logging message formatting
+    expressFormat: false, // default Express/morgan request formatting
+    colorize: true, // Color the text and status code
+    ignoreRoute: function (req, res) { return false } // skip some log messages
+  }))
+
+  // development error handler, will print stacktrace
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.send({
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler, no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.send({
+    message: err.message,
+    error: {}
+  })
+})
+
+// -----------------------------------------------------------------------------
+// CONFIGURE ROUTERS
 // -----------------------------------------------------------------------------
 
 // PASSPORT
