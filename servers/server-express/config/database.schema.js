92d1a588286378e683d36842f98bc45f358f556f
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-16 16:14:06 +0700

    Refine database.schema

--- servers/server-express/config/database.schema.js
+++ servers/server-express/config/database.schema.js
@@ -1,21 +1,22 @@
-const DATABASE_NAME = 'super-workshop-js'
+const DATABASE_NAME = 'super_workshop'
 
 const NEDB_PATH = './data/' + DATABASE_NAME
 
+const MONGODB_PROTOCOL = process.env.MONGODB_PROTOCOL || 'mongodb'
 const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost'
 const MONGODB_PORT = process.env.MONGODB_PORT || '27017'
 const MONGODB_DATABASE = process.env.DATABASE_NAME || DATABASE_NAME
 const MONGODB_USER = process.env.MONGODB_USER || ''
 const MONGODB_PASS = process.env.MONGODB_PASS || ''
 
-const MONGODB_ATLAS_KEY = process.env.MONGODB_ATLAS_KEY || ``
+const MONGODB_ATLAS_KEY = `change_this`
 
 let MONGODB_URI = ''
 
 if (MONGODB_USER === '') {
-  MONGODB_URI = process.env.MONGODB_URI || `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`
+  MONGODB_URI = process.env.MONGODB_URI || `${MONGODB_PROTOCOL}://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`
 } else if (MONGODB_USER !== '') {
-  MONGODB_URI = process.env.MONGODB_URI || `mongodb://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`
+  MONGODB_URI = process.env.MONGODB_URI || `${MONGODB_PROTOCOL}://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`
 }
 
 module.exports = {
