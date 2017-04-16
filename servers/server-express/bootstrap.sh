ad984fc2c56806b2c6421883333f442cffd073fb
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-16 16:13:04 +0700

    Create bootstrap.sh to ease config preparation

---
+++ servers/server-express/bootstrap.sh
@@ -0,0 +1,13 @@
+cp .env.schema .env
+cd config
+cp connection.schema.js connection.js
+cp credential.schema.js credential.js
+cp database.schema.js database.js
+cd ..
+
+# edit these files
+
+# .env
+# config/connection.js
+# config/credential.js
+# config/database.js
